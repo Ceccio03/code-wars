fa210548f267e9740113860b0375e68fb44d3938
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-27 09:12:38 +0200

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,5 +1,10 @@
 function removeAllSpaces(str) {
-    let nospace = str.replace(/ /g, '');
+    // let nospace = str.replace(/ /g, '');
+
+    // return nospace;
+
+    const wordArray = str.split(' ');
+    const nospace = wordArray.join('');
 
     return nospace;
 }
