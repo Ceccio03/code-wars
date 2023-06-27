1e822c483c785eac115af86a74951bfa80d90dc0
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-06-27 09:27:45 +0200

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -11,3 +11,19 @@ function removeAllSpaces(str) {
 console.log(removeAllSpaces("pippo") === "pippo");
 console.log(removeAllSpaces("   ") === "");
 console.log(removeAllSpaces("le mutande di mia nonna") === "lemutandedimianonna");
+
+function calculateBMI(weight, height) {
+    const proportion = weight / (height ** 2);
+
+    if (proportion <= 18.5) {
+        return 'Underweight';
+    } else if (proportion > 18.5 && proportion <= 25) {
+        return 'Normal';
+    } else if (proportion > 25 && proportion <= 30) {
+        return 'Overweight';
+    } else {
+        return 'Obese';
+    }
+}
+console.log(calculateBMI(170, 1.60) === 'Obese');
+console.log(calculateBMI(57, 1.66) === 'Normal');
